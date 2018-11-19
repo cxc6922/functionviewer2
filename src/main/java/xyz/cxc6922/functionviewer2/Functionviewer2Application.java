3175992adc8262d5bebd3ba3ddd7890df6efaf04
chenxiaochen <13951636922@qq.com>
2018-11-19 23:29:58 +0800

    Initial commit

---
+++ src/main/java/xyz/cxc6922/functionviewer2/Functionviewer2Application.java
@@ -0,0 +1,12 @@
+package xyz.cxc6922.functionviewer2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Functionviewer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Functionviewer2Application.class, args);
+    }
+}
